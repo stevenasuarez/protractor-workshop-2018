2bec03ac32afc95f0bb41d1a98d933a7953c90e7
Steven Suarez Castro <SSuarezC@pslcol.com.co>
2018-04-20 11:32:43 -0500

    migration to ts

---
+++ protractor/config.ts
@@ -0,0 +1,11 @@
+import { browser, Config } from 'protractor';
+
+export const config: Config = {
+	framework: 'jasmine',
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	specs: ['../test/Google.spec.js'],
+	noGlobals: true,
+	onPrepare: () => {
+		browser.ignoreSynchronization = true;
+	}
+}
