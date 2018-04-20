b0fec9771ad670261cd47cf0e75e35477b85f10e
Steven Suarez Castro <SSuarezC@pslcol.com.co>
2018-04-20 14:24:14 -0500

    chrome headless added

---
+++ protractor/headless.conf.ts
@@ -0,0 +1,19 @@
+import { browser, Config } from 'protractor';
+import { reporter }   from './helpers/reporter';
+
+export const config: Config = {
+	framework: 'jasmine',
+	specs: ['../test/Google.spec.js'],
+	noGlobals: true,
+	SELENIUM_PROMISE_MANAGER: false,
+	onPrepare: () => {
+		browser.ignoreSynchronization = true;
+		reporter();
+	},
+	capabilities: {
+		browserName: 'chrome',
+		chromeOptions: {
+			args: ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox']
+		}
+	}
+}
